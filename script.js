d90d927610548eaa640ec6c8b76f9c92b9d16856
ゆう <halloween_1031@hotmail.co.jp>
2019-06-28 17:47:52 +0900

    基礎

---
+++ script.js
@@ -0,0 +1,29 @@
+$(function () {
+    $('#ageSlider').slider({
+        max: 80,
+        min: 20,
+        orientation: "vertical",
+        step: 10,
+        value: 20,
+        range: "min",
+
+        slide: function (select, handleIndex) {
+            console.log(handleIndex.value);
+            document.getElementById("age").innerText = handleIndex.value;
+        }
+    });
+    $('#yearSlider').slider({
+        max: 2019,
+        min: 2011,
+        orientation: "horizontal",
+        step: 1,
+        value: 2011,
+        range: "min",
+
+        slide: function (select, handleIndex) {
+            console.log(handleIndex.value);
+            document.getElementById("year").innerText = handleIndex.value;
+        }
+    });
+
+});
