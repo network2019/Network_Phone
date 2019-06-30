fbde48ef51b5cf3597ab6875bb862ada801a6d3e
ゆう <halloween_1031@hotmail.co.jp>
2019-06-30 17:49:59 +0900

    6/30

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
+var data = []; //0-6 Smartphone , 7-13 Galapagos
+
+function getCSV(filename) {
+    var req = new XMLHttpRequest();
+    req.open('get', filename, true);
+    req.send(null);
+    req.onload = function () {
+        setCSV(req.responseText);
+    };
+}
+
+function setCSV(str) {
+    var dataArr;
+    var tmp = str.split('\n');
+    tmp.forEach(x => {
+        dataArr = x.split(',');
+        // if (dataArr[0]) {
+        //     data.push(dataArr.map(x => x.trim()));
+        // }
+        for (var i = 0; i < dataArr.length; i++) {
+            if (isNaN(Number(dataArr[i])) == false) {
+                dataArr[i] = Number(dataArr[i]);
+            }
+        }
+
+        data.push(dataArr);
+
+    });
+}
+getCSV("Smartphone.csv");
+getCSV("Galapagos.csv");
+
+
 $(function () {
+    console.table(data);
+    console.log(data[0][1]);
+
     $('#ageSlider').slider({
         max: 80,
         min: 20,
@@ -8,7 +44,6 @@ $(function () {
         range: "min",
 
         slide: function (select, handleIndex) {
-            console.log(handleIndex.value);
             document.getElementById("age").innerText = handleIndex.value;
         }
     });
@@ -21,9 +56,7 @@ $(function () {
         range: "min",
 
         slide: function (select, handleIndex) {
-            console.log(handleIndex.value);
             document.getElementById("year").innerText = handleIndex.value;
         }
     });
-
 });
